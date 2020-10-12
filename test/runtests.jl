3a7c3885de6dfb5b4e35a14ce9a18b2c4fcae5ea
Hannes <hasnep@gmail.com>
2020-10-12 22:47:59 +0100

    Add tests for summary

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,23 +2,37 @@ using CSV
 using DataSkimmer
 using RDatasets: dataset
 using Test
+using DataFrames
+using StructArrays
 
-iris = dataset("datasets", "iris")
-tuple_dataset = (A = [1, missing, 3], B = [1.0, 2.0, 3.0], C = ["hey", "there", "sailor"])
-iris_csv = CSV.File(joinpath("data", "example.csv"))
+iris_dataframe = dataset("datasets", "iris")
+iris_csv = CSV.File(joinpath("data", "iris.csv"))
+data_structarray =
+    StructArray([(A = 1, B = 2, C = 3), (A = 4, B = 5, C = 6), (A = 7, B = 8, C = 9)])
 datasets = Dict(
-    "iris dataframe" => iris,
+    "iris dataframe" => iris_dataframe,
     "iris csv" => iris_csv,
-    #   "tuple_dataset" => tuple_dataset
+    "structarray" => data_structarray,
 )
 @testset "DataSkimmer.jl" begin
-    @testset "Skims $ds_name" for (ds_name, dataset) in datasets
-        @test skim(dataset) isa DataSkimmer.Skimmed
-    end
     @testset "unicode_histogram" begin
         @testset "unicode_histogram draws $n_bars bars" for n_bars = 1:10
             @test DataSkimmer.unicode_histogram(iris.SepalLength, n_bars) isa String
             @test length(DataSkimmer.unicode_histogram(iris.SepalLength, n_bars)) == n_bars
+        end
+    end
+    @testset "$ds_name" for (ds_name, dataset) in datasets
+        @testset "Summary" begin
+            @testset "count_rows" begin
+                @test DataSkimmer.count_rows(dataset) isa Integer
+            end
+            @testset "count_columns" begin
+                @test DataSkimmer.count_columns(dataset) isa Integer
+            end
+        end
+        @testset "skim($ds_name)" begin
+            @test skim(dataset) isa DataSkimmer.Skimmed
+            println(skim(dataset))
         end
     end
 end
