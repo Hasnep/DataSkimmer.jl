9834a3e42df8f2d592e337cb3e3bfa9d535aad48
Hannes <hasnep@gmail.com>
2021-02-23 09:18:36 +0000

    Apply new formatting rules

--- src/histogram.jl
+++ src/histogram.jl
@@ -1,4 +1,6 @@
-"""Create a range of equally spaced steps between two Dates or DateTimes."""
+"""
+Create a range of equally spaced steps between two Dates or DateTimes.
+"""
 function Base.range(start::T, stop::T; length::Integer) where {T <: Union{Dates.Date, Dates.DateTime}}
     range_width = stop - start
     T_width = typeof(range_width)
@@ -7,7 +9,9 @@ function Base.range(start::T, stop::T; length::Integer) where {T <: Union{Dates.
     return start:step_width:stop
 end
 
-"""Construct a histogram made of a sequence of unicode bar characters."""
+"""
+Construct a histogram made of a sequence of unicode bar characters.
+"""
 function unicode_histogram(x, n_bins::Integer)::String
     n_nonmissing_datapoints = count(!ismissing, x)
     if n_nonmissing_datapoints == 0
@@ -18,8 +22,8 @@ function unicode_histogram(x, n_bins::Integer)::String
         max_value = maximum(x)
         bin_edges = range(min_value, max_value; length = n_bins + 1)
         weights = [
-            count(datapoint -> edge_lower < datapoint <= edge_upper, x)
-            for (edge_lower, edge_upper) in partition(bin_edges, 2, 1)
+            count(datapoint -> edge_lower < datapoint <= edge_upper, x) for
+            (edge_lower, edge_upper) in partition(bin_edges, 2, 1)
         ]
         weights_normalised = weights ./ n_nonmissing_datapoints
         bars = [w == 0 ? ' ' : Char(0x2581 + floor(Int, w * 8)) for w in weights]
