adc5fdb52a9edfc0ffedbb9982a6b333c802b928
Hannes <h@nnes.dev>
2021-07-11 19:43:02 +0100

    Clarify reasons for datasets to be included

--- test/datasets.jl
+++ test/datasets.jl
@@ -1,19 +1,30 @@
-iris_dataframe = RDatasets.dataset("datasets", "iris")
-
-base_table = [
-    (A = 1, B = "one", C = Dates.Date(2021, 1, 1)),
-    (A = 2, B = "two", C = Dates.Date(2021, 1, 2)),
-    (A = 3, B = "three", C = missing),
+# The most basic Tables.jl table is a vector of named tuples
+vector_of_named_tuples = [
+    (a = 1, b = "one", c = Dates.Date(2021, 1, 1)),
+    (a = 2, b = "two", c = Dates.Date(2021, 1, 2)),
+    (a = 3, b = "three", c = Dates.Date(2021, 1, 3)),
 ]
 
+# The famous iris dataset to test DataFrames.jl dataframes
+iris_dataframe = RDatasets.dataset("datasets", "iris")
+
+# Write the dataframe as CSV and then read again to test CSV.jl File
+iris_csv = CSV.File(seekstart(CSV.write(IOBuffer(), iris_dataframe)))
+
+# Use the economics dataset from ggplot2 to test datetimes
+economics_dataframe = RDatasets.dataset("ggplot2", "economics")
+
+# Convert the economics dataset to a TimeArray
+economics_timearray = TimeArray(economics_dataframe, timestamp = :Date)
+
 datasets = Dict(
+    "vector_of_named_tuples" => vector_of_named_tuples,
+    "named_tuple_of_vectors" => Tables.columntable(vector_of_named_tuples),
+    "struct_array" => StructArray(vector_of_named_tuples),
     "iris_dataframe" => iris_dataframe,
-    "iris_csv" => CSV.File(seekstart(CSV.write(IOBuffer(), iris_dataframe))),
-    "struct_array" => StructArray(base_table),
-    "timearray" =>
-        TimeArray(RDatasets.dataset("ggplot2", "economics"), timestamp = :Date),
+    "iris_csv" => iris_csv,
+    "economics_dataframe" => economics_dataframe,
+    "economics_timearray" => economics_timearray,
     "loomis_dataframe" => RDatasets.dataset("COUNT", "loomis"),
     "empty_dataframe" => DataFrame(x = [], y = Int64[], z = String[]),
-    "vector_of_nt" => base_table,
-    "nt_of_vectors" => Tables.columntable(base_table),
 )
